84c8c0d79fb91bcdb4e1b241efb4a485bc2aed22
Anton Cristo <antoncristo92@gmail.com>
2021-11-29 15:56:00 +0200

    initial

---
+++ src/components/introduction/introduction.tsx
@@ -0,0 +1,10 @@
+import classes from "./introduction.module.css";
+
+export const Introduction = () => {
+  return (
+    <div className={classes.introduction}>
+      <div>Hello, I'm Anton</div>
+      <div>Front-End Developer</div>
+    </div>
+  );
+};
