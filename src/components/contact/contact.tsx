a7bfffaf9a616f78eb90c072b79139b8e6bedea7
Anton Cristo <antoncristo92@gmail.com>
2021-12-09 10:11:18 +0200

    contact item component

--- src/components/contact/contact.tsx
+++ src/components/contact/contact.tsx
@@ -1,18 +1,88 @@
-import { RefObject, useRef } from "react";
+import { RefObject, useRef, MouseEvent } from "react";
+import { observer } from "mobx-react";
 import { useInitScrollMapRef } from "src/hooks";
-import { SectionHeader } from "src/shared";
-import { ScrollMenuMapName } from "src/stores";
+import { ResumeButton, SectionHeader } from "src/shared";
+import { ScrollMenuMapName, textShadowStore } from "src/stores";
+import phoneIcon from "src/assets/svg/phone.svg";
+import emailIcon from "src/assets/svg/email.svg";
+import githubIcon from "src/assets/svg/github.svg";
+import linkedinIcon from "src/assets/svg/linkedin.svg";
+import { textShadowActions } from "src/actions";
+
+import { ContactItem } from "./components";
+
+import classes from "./contact.module.css";
+import { ContactItem as ContactItemType } from "src/types";
 
 const SECTION_SCROLL_KEY: keyof ScrollMenuMapName = "CONTACT";
 
-export const Contact = () => {
+const contactItems: ContactItemType[] = [
+  {
+    contactInfo: "https://www.linkedin.com/in/anton-cristo-157652153/",
+    variant: "LINK",
+    icon: linkedinIcon,
+  },
+  {
+    contactInfo: "https://github.com/AntonCristo",
+    variant: "LINK",
+    icon: githubIcon,
+  },
+  {
+    contactInfo: "antoncristo92@gmail.com",
+    variant: "TEXT",
+    icon: emailIcon,
+  },
+  {
+    contactInfo: "054-3007046",
+    variant: "TEXT",
+    icon: phoneIcon,
+  },
+];
+
+export const Contact = observer(() => {
+  const { textShadowX, textShadowY, pauseEffect } = textShadowStore;
+
   const contactRef: RefObject<HTMLDivElement> = useRef(null);
+
+  const onMouseMoveHandler = (event: MouseEvent<HTMLDivElement>) => {
+    textShadowActions.textShadowCoordRelativeToMouse(
+      event.clientX,
+      event.clientY
+    );
+  };
+
+  const toggleTextShadowEffectOnMouseClick = (
+    event: MouseEvent<HTMLDivElement>
+  ) => {
+    pauseEffect && onMouseMoveHandler(event);
+    textShadowActions.shouldPauseTextShadowEffect(!pauseEffect);
+  };
 
   useInitScrollMapRef(SECTION_SCROLL_KEY, contactRef);
 
   return (
-    <div ref={contactRef}>
+    <div className={classes.contact} ref={contactRef}>
       <SectionHeader sectionTitle="CONTACT INFO" />
+      <div
+        onMouseMove={pauseEffect ? () => {} : onMouseMoveHandler}
+        onClick={toggleTextShadowEffectOnMouseClick}
+        className={classes.contactWrapper}
+      >
+        {contactItems.map((cItem, index) => (
+          <ContactItem key={JSON.stringify(cItem)} contactItem={cItem} />
+        ))}
+        <div className={classes.contactItem}>
+          <ResumeButton />
+        </div>
+        <div
+          style={{
+            filter: `drop-shadow(${textShadowX}px ${textShadowY}px 3px #000)`,
+          }}
+          className={classes.contactFooter}
+        >
+          Lets work together!
+        </div>
+      </div>
     </div>
   );
-};
+});
--- src/shared/resume-button/resume-button.tsx
+++ src/shared/resume-button/resume-button.tsx
@@ -1,3 +1,4 @@
+import { MouseEvent } from "react";
 import { awsS3controller } from "src/storage";
 
 import classes from "./resume-button.module.css";
@@ -16,7 +17,9 @@ export const ResumeButton = (props: ResumeButtonProps) => {
 
   const _isAbsolute = Object.keys(absoluteStyle).length;
 
-  const onResumeClickHandler = () => {
+  const onResumeClickHandler = (event: MouseEvent<HTMLDivElement>) => {
+    event.stopPropagation();
+
     if (!awsS3controller.resumePublicUrl) {
       alert(
         "Sorry, somthing is wrong with the button, try refreshing the resume :)"
