cb0ea57cbaa1c6d7473af95e935b1ea25417381b
Anton Cristo <antoncristo92@gmail.com>
2021-12-13 10:57:11 +0200

    font size fix, content fix

--- src/components/tech/tech.tsx
+++ src/components/tech/tech.tsx
@@ -25,16 +25,22 @@ export const Tech = observer(() => {
     <div ref={techRef} className={classes.tech}>
       <SectionHeader sectionTitle="Tech Skills" />
       <div className={classes.techSummary}>
-        <div>I'm mainly focused on client-side tech</div>
+        <div>Mainly focused on client-side tech</div>
         <div>Passionate about translating design to great UI/UX</div>
-        <div>Proficient In creating and maintaining the UI</div>
         <div>
-          Implemented RESTful API's with various requirements and 3rd party
-          API's
+          Proficient In creating and maintaining front-end functionality
         </div>
-        <div>Worked with SQL/NoSQL Databases</div>
-        <div>Constantly working on improving my skills and knowledge</div>
-        <div>Then share and discuss it with my team</div>
+        <div>
+          Understand back-end concepts and capable of assiting in related tasks
+        </div>
+        <div>
+          Implemented RESTful API's with various requirements, 3rd party API's
+          and Databases
+        </div>
+        <div>
+          Constantly working on improving my skills and knowledge, Then share
+          and discuss it with my team
+        </div>
       </div>
       <div className={classes.projAndTechItem}>
         {techStacks.map((techStackItem, index) => (
