d6e8168a404b982caee207a894e19f427c6a4450
Anton Cristo <antoncristo92@gmail.com>
2021-12-14 10:06:45 +0200

    store action to set screenshots by bucket prefix

--- src/components/projects/components/project-container/project-container.tsx
+++ src/components/projects/components/project-container/project-container.tsx
@@ -3,6 +3,7 @@ import { ProjectsScheme } from "src/types";
 import { Description, SlideShow } from "./components";
 
 import classes from "./project-container.module.css";
+import { awsS3controller } from "src/storage";
 
 type ProjectContainerProps = {
   project: ProjectsScheme;
@@ -11,6 +12,12 @@ type ProjectContainerProps = {
 
 export const ProjectContainer = (props: ProjectContainerProps) => {
   const { project, index } = props;
+
+  !project.screenshots &&
+    awsS3controller.getProjectAssetsPublicUrlFromS3ByPrefix(
+      project.storagePrefix
+    );
+
   return (
     <div className={classes.projectContainer}>
       <Description index={index} project={project} />
