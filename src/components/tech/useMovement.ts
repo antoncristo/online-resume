6217ffda3d1d27035e4f3290e9a008404af1dfb4
Anton Cristo <57062626+antoncristo@users.noreply.github.com>
2023-06-24 14:00:50 +0300

    tech section moving items (#30)
    * tech section moving items

--- src/components/tech/useMovement.ts
+++ src/components/tech/useMovement.ts
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 
-export const useMovement = () => {
+export const useMovement = (delta: { dx: number; dy: number }) => {
+	const { dx, dy } = delta;
 	const rafRef = useRef(0);
 	const [xAxisPosition, setXAxisPosition] = useState(0);
 	const [yAxisPosition, setYAxisPosition] = useState(0);
@@ -15,48 +16,48 @@ export const useMovement = () => {
 	const animate = useCallback(() => {
 		if (xAxisDirection.current === 'forward') {
 			if (xAxisPositionRef.current < 95) {
-				setXAxisPosition(prev => prev + 0.3);
+				setXAxisPosition(prev => prev + dx);
 			} else {
 				xAxisDirection.current = 'backward';
-				setXAxisPosition(prev => prev - 0.3);
+				setXAxisPosition(prev => prev - dx);
 			}
 		}
 
 		if (xAxisDirection.current === 'backward') {
 			if (xAxisPositionRef.current > 5) {
-				setXAxisPosition(prev => prev - 0.3);
+				setXAxisPosition(prev => prev - dx);
 			} else {
 				xAxisDirection.current = 'forward';
-				setXAxisPosition(prev => prev + 0.3);
+				setXAxisPosition(prev => prev + dx);
 			}
 		}
 
 		if (yAxisDirection.current === 'downward') {
 			if (yAxisPositionRef.current < 95) {
-				setYAxisPosition(prev => prev + 0.3);
+				setYAxisPosition(prev => prev + dy);
 			} else {
 				yAxisDirection.current = 'upward';
-				setXAxisPosition(prev => prev - 0.3);
+				setXAxisPosition(prev => prev - dy);
 			}
 		}
 
 		if (yAxisDirection.current === 'upward') {
 			if (yAxisPositionRef.current > 5) {
-				setYAxisPosition(prev => prev - 0.3);
+				setYAxisPosition(prev => prev - dy);
 			} else {
 				yAxisDirection.current = 'downward';
-				setYAxisPosition(prev => prev + 0.3);
+				setYAxisPosition(prev => prev + dy);
 			}
 		}
 
 		rafRef.current = requestAnimationFrame(animate);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
 	useEffect(() => {
 		rafRef.current = requestAnimationFrame(animate);
 
 		return () => cancelAnimationFrame(rafRef.current);
-
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
 
